805d05d416e6e4f8935e354b04f9385725e8280b
zaki-yama <shingoyamazaki00@gmail.com>
2016-07-12 02:37:54 +0900

    simple implementation for image load and preview

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+class ImagePreviewer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onImageSelected = this.onImageSelected.bind(this);
+  }
+
+  onImageSelected(e) {
+    const file = e.target.files[0];
+    const fr = new FileReader();
+
+    fr.onload = (event) => {
+      const imgNode = this.refs.image;
+      imgNode.src = event.target.result;
+    };
+    fr.readAsDataURL(file);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="file" accept="image/*" onChange={this.onImageSelected} />
+        <p>Image will be previewed here!</p>
+        <img ref="image" src="" />
+      </div>
+    );
+  }
+}
+
 render(
-  <div>hello</div>,
+  <ImagePreviewer />,
   document.getElementById('root')
 );
