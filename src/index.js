e7b181ecb0d52307b9e4f9e297b416261db01d85
zaki-yama <shingoyamazaki00@gmail.com>
2016-07-18 01:05:34 +0900

    resize & fix orientation using JavaScript-Load-Image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import loadImage from 'blueimp-load-image';
+
 class ImagePreviewer extends React.Component {
   constructor(props) {
     super(props);
@@ -10,38 +12,21 @@ class ImagePreviewer extends React.Component {
 
   onImageSelected(e) {
     const file = e.target.files[0];
-    const fr = new FileReader();
-
-    fr.onload = () => {
-      console.log('before resizing: ', fr.result.length);
-      // Image オブジェクトに src を指定すると、元画像の width と height が取れる
-      const img = new Image();
-      img.src = fr.result;
-      const width = img.width;
-      const height = img.height;
-      console.log('width, height = ', width, height);
-
-      // 縮小後のサイズを計算。ここでは横幅 (width) を指定
-      const dstWidth = 1024;
-      const scale = dstWidth / width;
-      const dstHeight = height * scale;
-
-      // Canvas オブジェクトを使い、縮小後の画像を描画
-      const canvas = document.createElement('canvas');
-      canvas.width = dstWidth;
-      canvas.height = dstHeight;
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(img, 0, 0, dstWidth, dstHeight);
-
-      // Canvas オブジェクトから Data URL を取得
-      const resized = canvas.toDataURL();
-      console.log('after resizing: ', resized.length);
-      console.log('width, height = ', dstWidth, dstHeight);
-
-      const imgNode = this.refs.image;
-      imgNode.src = resized;
-    };
-    fr.readAsDataURL(file);
+    loadImage.parseMetaData(file, (data) => {
+      const options = {
+        maxHeight: 1024,
+        maxWidth: 1024,
+        canvas: true
+      };
+      if (data.exif) {
+        options.orientation = data.exif.get('Orientation');
+      }
+      loadImage(file, (canvas) => {
+        const resized = canvas.toDataURL('image/jpeg');
+        const imgNode = this.refs.image;
+        imgNode.src = resized;
+      }, options);
+    });
   }
 
   render() {
